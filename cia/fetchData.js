1d2b6e73fd4345e17afef602b9fd2cd6f2729763
facmartoni <thunderxforce@gmail.com>
2020-04-24 17:33:00 -0300

    fetchData.js changed to the cloud

--- cia/fetchData.js
+++ cia/fetchData.js
@@ -2,7 +2,9 @@ let = $accordion = document.getElementById("accordion");
 
 let ciaData = null;
 
-fetch("./cia.json")
+fetch(
+  "https://app.scrapinghub.com/api/v2/datasets/kdybI1E79Ww/download?format=json"
+)
   .then((response) => {
     return response.json();
   })
